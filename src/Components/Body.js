8a70761dded40320d8953381d028a1ce15107b6e
Kevin Su <kevinsu@outlook.co.nz>
2021-09-09 18:39:57 +1200

    snake movement basic

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,30 +1,71 @@
 import React, {useState, useEffect} from 'react';
 
 const Body = () => {
-  const [snake, setSnake] = useState([]);
+    const [snake, setSnake] = useState([]);
 
-  useEffect(() => {
-    setSnake([[0,0], [0,2]])
-  }, [])
+    useEffect(() => {
+    setSnake([[25,25], [25,30], [25, 35]])
+    }, [])
 
-  const appendSnake = () => {
+    const appendSnake = () => {
     var temp = snake.slice();
     temp.push([0, 3]);
     setSnake(temp);
+    }
 
-  }
+    const appendRight = () => {
+    var x = snake.length;
+    var temp = snake.slice(1);
+    var lastCoord = snake.slice(x-1);
+    console.log(lastCoord);
+    var rightCoord = [lastCoord[0][0], lastCoord[0][1]+5];
+    console.log(rightCoord);
+    temp.push(rightCoord);
+    setSnake(temp);
+    }
 
-  return (
-      <>
-          <div className="board">
-            <div className="snake" style = {{top: "0%"}}></div>
-            <div className="snake" style = {{top: "0%", left:"5%"}}></div>
-            <div className="snake" style = {{top: "0%", left:"10%"}}></div>
-          </div>
-          <h1>{snake}</h1>
-          <button onClick={(e) => appendSnake(e)}>OnClick</button>
-      </>
-  );
+    const appendDown = () => {
+      var x = snake.length;
+      var temp = snake.slice(1);
+      var lastCoord = snake.slice(x-1);
+      console.log(lastCoord);
+      var downCoord = [lastCoord[0][0] + 5, lastCoord[0][1]];
+      console.log(downCoord);
+      temp.push(downCoord);
+      setSnake(temp);
+    }
+
+    const appendUp = () => {
+        var x = snake.length;
+        var temp = snake.slice(1);
+        var lastCoord = snake.slice(x-1);
+        console.log(lastCoord);
+        var upCoord = [lastCoord[0][0] - 5, lastCoord[0][1]];
+        console.log(upCoord);
+        temp.push(upCoord);
+        setSnake(temp);
+    }
+
+    const appendLeft = () => {
+        var x = snake.length;
+        var temp = snake.slice(1);
+        var lastCoord = snake.slice(x-1);
+        console.log(lastCoord);
+        var leftCoord = [lastCoord[0][0], lastCoord[0][1] -5];
+        console.log(leftCoord);
+        temp.push(leftCoord);
+        setSnake(temp);
+    }
+
+    return (
+        <>
+            <div className="board">
+            {snake.map(x => (<div className="snake" style={{top:`${x[0]}`+"%", left:`${x[1]}`+"%"}}></div>))}
+            </div>
+            <h1>{snake}</h1>
+            <button onClick={(e) => appendLeft(e)}>OnClick</button>
+        </>
+    );
 }
 
 export default Body;
