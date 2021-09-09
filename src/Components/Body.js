f81a94432cbf1b59d371d111ea7cbefa6c7c1e9f
Kevin Su <kevinsu@outlook.co.nz>
2021-09-09 18:59:47 +1200

    snake movement and rotation

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,37 +2,50 @@ import React, {useState, useEffect} from 'react';
 
 const Body = () => {
     const [snake, setSnake] = useState([]);
+    const [gameOver, setGameOver] = useState(false);
 
     useEffect(() => {
-    setSnake([[25,25], [25,30], [25, 35]])
+        setSnake([[25,25], [25,30], [25, 35]])
     }, [])
 
-    const appendSnake = () => {
-    var temp = snake.slice();
-    temp.push([0, 3]);
-    setSnake(temp);
-    }
-
     const appendRight = () => {
-    var x = snake.length;
-    var temp = snake.slice(1);
-    var lastCoord = snake.slice(x-1);
-    console.log(lastCoord);
-    var rightCoord = [lastCoord[0][0], lastCoord[0][1]+5];
-    console.log(rightCoord);
-    temp.push(rightCoord);
-    setSnake(temp);
+        var x = snake.length;
+        var temp = snake.slice(1);
+        var lastCoord = snake.slice(x-1);
+        console.log(lastCoord);
+        var rightCoord = [lastCoord[0][0], lastCoord[0][1]+5];
+        if(rightCoord[1] > 95){
+            rightCoord[1] = 0;
+        }
+        for (var i = 0; i < temp.length; i++){
+            var current = temp[i];
+            if(current[0] === rightCoord[0] && current[1] === rightCoord[1]){
+                setGameOver(true);
+            }
+        }
+        console.log(rightCoord);
+        temp.push(rightCoord);
+        setSnake(temp);
     }
 
     const appendDown = () => {
-      var x = snake.length;
-      var temp = snake.slice(1);
-      var lastCoord = snake.slice(x-1);
-      console.log(lastCoord);
-      var downCoord = [lastCoord[0][0] + 5, lastCoord[0][1]];
-      console.log(downCoord);
-      temp.push(downCoord);
-      setSnake(temp);
+        var x = snake.length;
+        var temp = snake.slice(1);
+        var lastCoord = snake.slice(x-1);
+        console.log(lastCoord);
+        var downCoord = [lastCoord[0][0] + 5, lastCoord[0][1]];
+        if(downCoord[0] > 95){
+            downCoord[0] = 0;
+        }
+        for (var i = 0; i < temp.length; i++){
+            var current = temp[i];
+            if(current[0] === downCoord[0] && current[1] === downCoord[1]){
+                setGameOver(true);
+            }
+        }
+        console.log(downCoord);
+        temp.push(downCoord);
+        setSnake(temp);
     }
 
     const appendUp = () => {
@@ -41,6 +54,15 @@ const Body = () => {
         var lastCoord = snake.slice(x-1);
         console.log(lastCoord);
         var upCoord = [lastCoord[0][0] - 5, lastCoord[0][1]];
+        if(upCoord[0] < 0){
+            upCoord[0] = 95;
+        }
+        for (var i = 0; i < temp.length; i++){
+            var current = temp[i];
+            if(current[0] === upCoord[0] && current[1] === upCoord[1]){
+                setGameOver(true);
+            }
+        }
         console.log(upCoord);
         temp.push(upCoord);
         setSnake(temp);
@@ -50,8 +72,18 @@ const Body = () => {
         var x = snake.length;
         var temp = snake.slice(1);
         var lastCoord = snake.slice(x-1);
-        console.log(lastCoord);
         var leftCoord = [lastCoord[0][0], lastCoord[0][1] -5];
+        if(leftCoord[1] < 0){
+            leftCoord[1] = 95;
+        }
+        console.log(leftCoord);
+        console.log(temp);
+        for (var i = 0; i < temp.length; i++){
+            var current = temp[i];
+            if(current[0] === leftCoord[0] && current[1] === leftCoord[1]){
+                setGameOver(true);
+            }
+        }
         console.log(leftCoord);
         temp.push(leftCoord);
         setSnake(temp);
@@ -62,7 +94,7 @@ const Body = () => {
             <div className="board">
             {snake.map(x => (<div className="snake" style={{top:`${x[0]}`+"%", left:`${x[1]}`+"%"}}></div>))}
             </div>
-            <h1>{snake}</h1>
+            <h1>{gameOver ? "Game Over" : "Good Luck"}</h1>
             <button onClick={(e) => appendLeft(e)}>OnClick</button>
         </>
     );
