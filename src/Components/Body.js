63bc6b4d0fd1a32059e2d6a12ccb889ca6a0f562
Kevin Su <kevinsu@outlook.co.nz>
2021-09-09 18:20:17 +1200

    experimental stuff

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-function Body() {
+const Body = () => {
+  const [snake, setSnake] = useState([]);
+
+  useEffect(() => {
+    setSnake([[0,0], [0,2]])
+  }, [])
+
+  const appendSnake = () => {
+    var temp = snake.slice();
+    temp.push([0, 3]);
+    setSnake(temp);
+
+  }
+
   return (
-      <div className="board">
-        <div className="snake" style = {{top: "0%"}}></div>
-        <div className="snake" style = {{top: "0%", left:"5%"}}></div>
-        <div className="snake" style = {{top: "0%", left:"10%"}}></div>
-      </div>
+      <>
+          <div className="board">
+            <div className="snake" style = {{top: "0%"}}></div>
+            <div className="snake" style = {{top: "0%", left:"5%"}}></div>
+            <div className="snake" style = {{top: "0%", left:"10%"}}></div>
+          </div>
+          <h1>{snake}</h1>
+          <button onClick={(e) => appendSnake(e)}>OnClick</button>
+      </>
   );
 }
 
