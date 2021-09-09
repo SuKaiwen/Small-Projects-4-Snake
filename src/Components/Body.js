908f1026d499b2c09dd5e0650caf695346f381fe
Kevin Su <kevinsu@outlook.co.nz>
2021-09-09 19:37:15 +1200

    refactor into class component and added setInterval

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,103 +1,120 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef, Component} from 'react';
 
-const Body = () => {
-    const [snake, setSnake] = useState([]);
-    const [gameOver, setGameOver] = useState(false);
+class Body extends Component{
 
-    useEffect(() => {
-        setSnake([[25,25], [25,30], [25, 35]])
-    }, [])
+    constructor(props){
+        super(props);
 
-    const appendRight = () => {
-        var x = snake.length;
-        var temp = snake.slice(1);
-        var lastCoord = snake.slice(x-1);
-        console.log(lastCoord);
-        var rightCoord = [lastCoord[0][0], lastCoord[0][1]+5];
-        if(rightCoord[1] > 95){
-            rightCoord[1] = 0;
+        this.state={
+            snake: [[25,25], [25,30], [25, 35]],
+            gameOver: false,
+            start: false,
+            direction: "right",
+        };
+    }
+
+    componentDidMount(){
+        this.interval = setInterval(() => this.appendDown(), 500);
+    }
+
+    appendRight(){
+        var x = this.state.snake.length;
+        var temp = this.state.snake.slice(1);
+        var lastCoord = this.state.snake.slice(x-1);
+        var nextCoord = [lastCoord[0][0], lastCoord[0][1]+5];
+        if(nextCoord[1] > 95){
+            nextCoord[1] = 0;
         }
         for (var i = 0; i < temp.length; i++){
             var current = temp[i];
-            if(current[0] === rightCoord[0] && current[1] === rightCoord[1]){
-                setGameOver(true);
+            if(current[0] === nextCoord[0] && current[1] === nextCoord[1]){
+                this.setState({
+                    gameOver: true,
+                });
             }
         }
-        console.log(rightCoord);
-        temp.push(rightCoord);
-        setSnake(temp);
+        temp.push(nextCoord);
+        this.setState({
+            snake: temp,
+        });
     }
 
-    const appendDown = () => {
-        var x = snake.length;
-        var temp = snake.slice(1);
-        var lastCoord = snake.slice(x-1);
-        console.log(lastCoord);
-        var downCoord = [lastCoord[0][0] + 5, lastCoord[0][1]];
-        if(downCoord[0] > 95){
-            downCoord[0] = 0;
+    appendLeft(){
+        var x = this.state.snake.length;
+        var temp = this.state.snake.slice(1);
+        var lastCoord = this.state.snake.slice(x-1);
+        var nextCoord = [lastCoord[0][0], lastCoord[0][1]-5];
+        if(nextCoord[1] < 0){
+            nextCoord[1] = 95;
         }
         for (var i = 0; i < temp.length; i++){
             var current = temp[i];
-            if(current[0] === downCoord[0] && current[1] === downCoord[1]){
-                setGameOver(true);
+            if(current[0] === nextCoord[0] && current[1] === nextCoord[1]){
+                this.setState({
+                    gameOver: true,
+                });
             }
         }
-        console.log(downCoord);
-        temp.push(downCoord);
-        setSnake(temp);
+        temp.push(nextCoord);
+        this.setState({
+            snake: temp,
+        });
     }
 
-    const appendUp = () => {
-        var x = snake.length;
-        var temp = snake.slice(1);
-        var lastCoord = snake.slice(x-1);
-        console.log(lastCoord);
-        var upCoord = [lastCoord[0][0] - 5, lastCoord[0][1]];
-        if(upCoord[0] < 0){
-            upCoord[0] = 95;
+    appendUp(){
+        var x = this.state.snake.length;
+        var temp = this.state.snake.slice(1);
+        var lastCoord = this.state.snake.slice(x-1);
+        var nextCoord = [lastCoord[0][0]-5, lastCoord[0][1]];
+        if(nextCoord[0] < 0){
+            nextCoord[0] = 95;
         }
         for (var i = 0; i < temp.length; i++){
             var current = temp[i];
-            if(current[0] === upCoord[0] && current[1] === upCoord[1]){
-                setGameOver(true);
+            if(current[0] === nextCoord[0] && current[1] === nextCoord[1]){
+                this.setState({
+                    gameOver: true,
+                });
             }
         }
-        console.log(upCoord);
-        temp.push(upCoord);
-        setSnake(temp);
+        temp.push(nextCoord);
+        this.setState({
+            snake: temp,
+        });
     }
 
-    const appendLeft = () => {
-        var x = snake.length;
-        var temp = snake.slice(1);
-        var lastCoord = snake.slice(x-1);
-        var leftCoord = [lastCoord[0][0], lastCoord[0][1] -5];
-        if(leftCoord[1] < 0){
-            leftCoord[1] = 95;
+    appendDown(){
+        var x = this.state.snake.length;
+        var temp = this.state.snake.slice(1);
+        var lastCoord = this.state.snake.slice(x-1);
+        var nextCoord = [lastCoord[0][0]+5, lastCoord[0][1]];
+        if(nextCoord[0] > 95){
+            nextCoord[0] = 0;
         }
-        console.log(leftCoord);
-        console.log(temp);
         for (var i = 0; i < temp.length; i++){
             var current = temp[i];
-            if(current[0] === leftCoord[0] && current[1] === leftCoord[1]){
-                setGameOver(true);
+            if(current[0] === nextCoord[0] && current[1] === nextCoord[1]){
+                this.setState({
+                    gameOver: true,
+                });
             }
         }
-        console.log(leftCoord);
-        temp.push(leftCoord);
-        setSnake(temp);
+        temp.push(nextCoord);
+        this.setState({
+            snake: temp,
+        });
     }
 
-    return (
-        <>
-            <div className="board">
-            {snake.map(x => (<div className="snake" style={{top:`${x[0]}`+"%", left:`${x[1]}`+"%"}}></div>))}
-            </div>
-            <h1>{gameOver ? "Game Over" : "Good Luck"}</h1>
-            <button onClick={(e) => appendLeft(e)}>OnClick</button>
-        </>
-    );
+    render(){
+        return (
+            <>
+                <div className="board">
+                {this.state.snake.map(x => (<div className="snake" style={{top:`${x[0]}`+"%", left:`${x[1]}`+"%"}}></div>))}
+                </div>
+                <h1>{this.state.gameOver ? "Game Over" : "Good Luck"}</h1>
+            </>
+        );
+    };
 }
 
 export default Body;
